611d48976a52d7ce185b0418b4d82accd03e51eb
João Vitor Souza <jvcsouza12@gmail.com>
2023-09-17 18:19:36 -0300

    .

--- src/services/api/config.ts
+++ src/services/api/config.ts
@@ -1,5 +1,5 @@
-export const urldefault = 'http://localhost:8181';
-export const apiAuthenticated = 'http://localhost:8001'
+// export const urldefault = 'http://localhost:8181';
+// export const apiAuthenticated = 'http://localhost:8001'
 
-// export const urldefault = 'https://stockalertapi.onrender.com';
-// export const apiAuthenticated = 'https://stockalertauthapi.onrender.com'
+export const urldefault = 'https://stockalertapi.onrender.com';
+export const apiAuthenticated = 'https://stockalertauthapi.onrender.com'
