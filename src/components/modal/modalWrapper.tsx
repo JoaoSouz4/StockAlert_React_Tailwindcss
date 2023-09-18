063d7e1af0f57ea8ebe9713cdb978a3829177621
João Vitor Souza <jvcsouza12@gmail.com>
2023-09-18 01:13:22 -0300

    .

--- src/components/modal/modalWrapper.tsx
+++ src/components/modal/modalWrapper.tsx
@@ -2,7 +2,7 @@ import {ReactNode} from 'react';
 
 export function ModalWrapper({children}: {children: ReactNode}){
     return (
-        <div className = ' z-40 fixed top-0 p-8 left-0 w-full h-full bg-[rgb(32,32,32,10%)]'>
+        <div className = ' z-40 fixed top-0 p-8 left-0 w-full h-full bg-[rgb(32,32,32,35%)]'>
             <div className = 'z-40 flex justify-center items-center h-full'>
                 {children}
             </div>
