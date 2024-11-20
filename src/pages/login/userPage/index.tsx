2ab9e81817d15f77399d9c5564d1c2dce52561c3
João Vitor Souza <joaosouz8@gmail.com>
2024-11-19 23:06:25 -0400

    .

--- src/pages/login/userPage/index.tsx
+++ src/pages/login/userPage/index.tsx
@@ -2,15 +2,42 @@ import { Option } from "./option"
 import {FaGear} from 'react-icons/fa6'
 import {ImExit} from 'react-icons/im'
 import { AuthContext } from "../../../context/AuthContext";
-import {useContext} from 'react';
+import {useContext, useState} from 'react';
 import { BiSolidUser } from "react-icons/bi";
 import { useNavigate } from "react-router-dom";
 import { VscFilePdf } from "react-icons/vsc";
+import { urldefault } from "../../../services/api/config";
+import Spinner from '../../../components/spinner';
 
 export function UserPage(){
 
     const {authState, AuthActions} = useContext(AuthContext);
     const navigate = useNavigate();
+    const [isLoading, setIsLoading] = useState(false)
+
+    function handlePdf() {
+        setIsLoading(true);
+        fetch(`${urldefault}/get/getpdf`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Falha na requisição PDF');
+                }
+                return response.blob();  // Convertendo a resposta em um Blob (arquivo binário)
+            })
+            .then(blob => {
+                const url = window.URL.createObjectURL(blob); // Cria um URL temporário para o arquivo
+                const link = document.createElement('a'); // Cria um link para o download
+                link.href = url; 
+                link.download = 'listadefaltabh.pdf'; // Define o nome do arquivo
+                link.click();  // Simula o clique no link, forçando o download
+                window.URL.revokeObjectURL(url); // Libera o URL temporário após o uso
+                setIsLoading(false);
+            })
+            .catch(error => {
+                setIsLoading(false);
+                console.error('Erro ao gerar PDF:', error);
+            });
+    }
 
     return (
         <div className = 'flex justify-center items-center h-full w-full'>
@@ -32,11 +59,18 @@ export function UserPage(){
                         onAction = {() => {navigate('/updatepass')}}
                         icon = {FaGear}
                     />
-                    <Option
-                        nameOption = 'Gerar PDF'
-                        onAction = {() => console.log('')}
-                        icon = {VscFilePdf}
-                    />
+                    {
+                        isLoading?
+                            <div className = 'shadow-sm border-2 border-slate-200 rounded-2xl p-4'>
+                                <Spinner />
+                            </div>
+                        :
+                            <Option
+                            nameOption = 'Gerar PDF'
+                            onAction = {handlePdf}
+                            icon = {VscFilePdf}
+                            />
+                    }
                     <Option
                         nameOption = 'Sair'
                         onAction = {() => {AuthActions.logout()}}
